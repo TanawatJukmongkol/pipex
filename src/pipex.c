04189379fa9149afbb110a6e2e400f867cc7b966
TanawatJukmongkol <tanawat.jukmon@gmail.com>
2023-03-13 13:10:54 +0700

    ughhh

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: tjukmong <tjukmong@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 21:42:33 by tjukmong          #+#    #+#             */
-/*   Updated: 2023/03/13 07:04:51 by tjukmong         ###   ########.fr       */
+/*   Updated: 2023/03/13 13:07:37 by tjukmong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,9 @@ void	exec(t_global *g, char **cmd)
 {
 	char	*err_msg;
 	char	*path;
+	size_t	indx;
 
+	indx = 0;
 	path = get_path(g, cmd[0]);
 	execve(path, cmd, g->envp);
 	err_msg = ft_strjoin("\033[91mPipeX:\033[0m ", cmd[0]);
@@ -46,6 +48,12 @@ void	exec(t_global *g, char **cmd)
 		ft_putstr_fd(err_msg, 2);
 		ft_putendl_fd(": command not found", 2);
 	}
+	while (cmd[indx])
+	{
+		free(cmd[indx]);
+		indx++;
+	}
+	free(cmd);
 	free(err_msg);
 	free(path);
 	exit(127);
@@ -54,6 +62,14 @@ void	exec(t_global *g, char **cmd)
 void	redirect_infile(t_global *g)
 {
 	pid_t	file;
+
+	// for (size_t i = 1; i < g->nproc; i++)
+	// {
+	// 	if (i == g->proc.indx)
+	// 		continue ;
+	// 	close(g->pipes[i][0]);
+	// 	close(g->pipes[i][1]);
+	// }
 
 	file = open_file(g, g->argv[g->proc.indx], P_READ);
 	if (file < 0)
@@ -65,11 +81,23 @@ void	redirect_infile(t_global *g)
 
 	dup2(file, 0);
 	close(file);
-	exec(g, (char *[]){g->argv[g->proc.indx + 1], NULL});
+	exec(g, ft_split(g->argv[g->proc.indx + 1], ' '));
 }
 
 void	pipe_and_execute(t_global *g)
 {
+
+	for (size_t i = 1; i < g->nproc; i++)
+	{
+		if (i == g->proc.indx - 1 || i == g->proc.indx - 2)
+			continue ;
+		// close(g->pipes[i][0]);
+		// close(g->pipes[i][1]);
+	}
+
+	close(g->pipes[0][1]);
+	close(g->pipes[3][1]);
+
 	close(g->pipes[g->proc.indx - 2][1]);
 	dup2(g->pipes[g->proc.indx - 2][0], 0);
 	close(g->pipes[g->proc.indx - 2][0]);
@@ -77,12 +105,20 @@ void	pipe_and_execute(t_global *g)
 	close(g->pipes[g->proc.indx - 1][0]);
 	dup2(g->pipes[g->proc.indx - 1][1], 1);
 	close(g->pipes[g->proc.indx - 1][1]);
-	exec(g, (char *[]){g->argv[g->proc.indx + 1], NULL});
+	exec(g, ft_split(g->argv[g->proc.indx + 1], ' '));
 }
 
 void	redirect_outfile(t_global *g)
 {
 	pid_t	out;
+
+	for (size_t i = 1; i < g->nproc; i++)
+	{
+		if (i == g->proc.indx - 2)
+			continue ;
+		close(g->pipes[i][0]);
+		close(g->pipes[i][1]);
+	}
 
 	out = open_file(g, g->argv[g->proc.indx + 2], P_WRITE);
 	if (out < 0)
@@ -90,15 +126,12 @@ void	redirect_outfile(t_global *g)
 
 	// printf("%s", get_next_line(g->pipes[0][0]));
 
-	close(g->pipes[g->proc.indx - 3][0]);
-	close(g->pipes[g->proc.indx - 3][1]);
-
 	close(g->pipes[g->proc.indx - 2][1]);
 	dup2(g->pipes[g->proc.indx - 2][0], 0);
 	close(g->pipes[g->proc.indx - 2][0]);
 	dup2(out, 1);
 	close(out);
-	exec(g, (char *[]){g->argv[g->proc.indx + 1], NULL});
+	exec(g, ft_split(g->argv[g->proc.indx + 1], ' '));
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -122,8 +155,8 @@ int	main(int argc, char **argv, char **envp)
 		assign_task(&g, indx, pipe_and_execute);
 	assign_task(&g, indx, redirect_outfile);
 
-	indx = 1;
-	while (indx < g.argc - 4)
+	indx = 0;
+	while (indx < g.argc - 3)
 	{
 		waitpid(g.pids[indx], &exit_stat, 0);
 		indx++;
